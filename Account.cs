3b81fa7468da9eb80176b7f5ebbeaac8dbf6f131
mertacun <acun0008@algonquinlive.com>
2024-05-08 21:30:13 -0400

    Create a bank console application for opening and maintaining saving accounts for customers

---
+++ Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Account
+    {
+        public int AccountNumber { get; }
+        public string OwnerName { get; }
+        public double Balance { get; private set; }
+        public double MonthlyDepositAmount { get; }
+
+        public static double MonthlyFee { get; } = 4.0;
+        public static double MonthlyInterestRate { get; } = 0.0025;
+        public static double MinimumInitialBalance { get; } = 1000;
+        public static double MinimumMonthDeposit { get; } = 50;
+
+        private static Random random = new Random();
+
+        public Account(string ownerName, double initialDeposit, double monthlyDepositAmount)
+        {
+            AccountNumber = random.Next(90000, 100000);
+            OwnerName = ownerName;
+            Balance = initialDeposit;
+            MonthlyDepositAmount = monthlyDepositAmount;
+        }
+
+        public void DeductMonthlyFee()
+        {
+            Balance -= MonthlyFee;
+        }
+
+        public void AddMonthlyInterest()
+        {
+            Balance += Balance * MonthlyInterestRate;
+        }
+
+        public void AddMonthlyDeposit()
+        {
+            Balance += MonthlyDepositAmount;
+        }
+
+        public double BalanceAfterMonths(int months)
+        {
+            for (int i = 0; i < months; i++)
+            {
+                DeductMonthlyFee();
+                AddMonthlyInterest();
+                AddMonthlyDeposit();
+            }
+            return Balance;
+        }
+    }
+}
---
+++ Bank.cs
@@ -0,0 +1,57 @@
+﻿
+using Lab4;
+using System;
+using System.Collections.Generic;
+
+class Bank
+{
+    static void Main()
+    {
+        List<Account> accounts = new List<Account>();
+
+
+        Console.Write("Enter the number of months to deposit: ");
+        int months;
+        while (!int.TryParse(Console.ReadLine(), out months) || months <= 0)
+        {
+            Console.WriteLine("Invalid input! Please enter a valid number of months.");
+            Console.Write("Enter the number of months to deposit: ");
+        }
+
+        while (true)
+        {
+            Console.Write("\nEnter Customer Name: ");
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name))
+                break;
+
+            Console.Write($"Enter {name}'s Initial Deposit Amount (minimum $1,000.00): ");
+            double initialDeposit;
+            while (!double.TryParse(Console.ReadLine(), out initialDeposit) || initialDeposit < Account.MinimumInitialBalance)
+            {
+                Console.WriteLine($"Initial deposit amount should be at least ${Account.MinimumInitialBalance}.");
+                Console.Write($"Enter {name}'s Initial Deposit Amount (minimum $1,000.00): ");
+            }
+
+            Console.Write($"Enter {name}'s Monthly Deposit Amount (minimum $50.00): ");
+            double monthlyDeposit;
+            while (!double.TryParse(Console.ReadLine(), out monthlyDeposit) || monthlyDeposit < Account.MinimumMonthDeposit)
+            {
+                Console.WriteLine($"Monthly deposit amount should be at least ${Account.MinimumMonthDeposit}.");
+                Console.Write($"Enter {name}'s Monthly Deposit Amount (minimum $50.00): ");
+            }
+
+            Account account = new Account(name, initialDeposit, monthlyDeposit);
+            accounts.Add(account);
+        }
+        Console.WriteLine();
+        foreach (var account in accounts)
+        {
+            Console.WriteLine($"After {months} months, {account.OwnerName}'s account (#{account.AccountNumber}), has a balance of: ${account.BalanceAfterMonths(months):F2}");
+        }
+        Console.WriteLine("\nPress Enter to complete");
+        Console.ReadLine();
+    }
+}
+
